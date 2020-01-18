8ebb35ad19435104376f97b5cd3dbacb2c29de13
Hamza <hamza.dahmoun7@gmail.com>
2020-01-18 21:52:55 +0100

    add setTimeout() inside projectTouchStart() to wait for 1 second before running

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function hideMenu() {
     document.querySelector("#navbar i").addEventListener("click", showMenu);
 }
 let menuLinks = document.querySelectorAll("#navbar a");
-for(let i=0; i<menuLinks.length; i++){
+for (let i = 0; i < menuLinks.length; i++) {
     menuLinks[i].addEventListener("click", hideMenu);
 }
 /********************************************* PROJECTS HOVER EFFECT SIMULATION IN MOBILE DEVICE */
@@ -30,18 +30,22 @@ for (let i = 0; i < projects.length; i++) {
 
 function projectTouchStart(event) {
     //this function makes the project container elements transparent and displays the project description and links
-    //1- make touched elements transparent
-    let project_tile = event.target.closest(".project-tile");//bringing the nearest element that have a class 'project-tile'
-    let project_name_container = event.target.closest(".project-tile").querySelector(".project-name-container");//bringing the nearest element that have a class 'project-name-container'
-    let project_picture = event.target.closest(".project-tile").querySelector(".project-picture");//bringing the nearest element that have a class 'project-picture'
-    let technologies_in_project_container = event.target.closest(".project-tile").querySelector(".technologies-in-project-container");//bringing the nearest element that have a class 'technologies-in-project-container'    
-    project_tile.style.boxShadow = "0px 0px 10px var(--cool-gray)";
-    project_name_container.style.opacity = 0.1;
-    project_picture.style.opacity = 0.1;
-    technologies_in_project_container.style.opacity = 0.1;
-    //2- display hiden elements
-    let project_description_and_buttons_container = event.target.closest(".project-tile").querySelector(".project-description-and-buttons-container");//bringing the nearest element that have a class 'project-description-and-buttons-container'    
-    project_description_and_buttons_container.style.opacity = 1;    
+    // but it waits for one second to do so ... for better UX
+    setTimeout(function () {
+        //1- make touched elements transparent
+        let project_tile = event.target.closest(".project-tile");//bringing the nearest element that have a class 'project-tile'
+        let project_name_container = event.target.closest(".project-tile").querySelector(".project-name-container");//bringing the nearest element that have a class 'project-name-container'
+        let project_picture = event.target.closest(".project-tile").querySelector(".project-picture");//bringing the nearest element that have a class 'project-picture'
+        let technologies_in_project_container = event.target.closest(".project-tile").querySelector(".technologies-in-project-container");//bringing the nearest element that have a class 'technologies-in-project-container'    
+        project_tile.style.boxShadow = "0px 0px 10px var(--cool-gray)";
+        project_name_container.style.opacity = 0.1;
+        project_picture.style.opacity = 0.1;
+        technologies_in_project_container.style.opacity = 0.1;
+        //2- display hiden elements
+        let project_description_and_buttons_container = event.target.closest(".project-tile").querySelector(".project-description-and-buttons-container");//bringing the nearest element that have a class 'project-description-and-buttons-container'    
+        project_description_and_buttons_container.style.opacity = 1;
+    }, 1000);
+
 }
 function projectTouchEnd(event) {
     //this function makes the project container elements opacity=1 and hide the project description and links
@@ -56,6 +60,6 @@ function projectTouchEnd(event) {
     technologies_in_project_container.style.opacity = 1;
     //2- hide the project description and links
     let project_description_and_buttons_container = event.target.closest(".project-tile").querySelector(".project-description-and-buttons-container");//bringing the nearest element that have a class 'project-description-and-buttons-container'    
-    project_description_and_buttons_container.style.opacity = 0;   
+    project_description_and_buttons_container.style.opacity = 0;
 }
 
